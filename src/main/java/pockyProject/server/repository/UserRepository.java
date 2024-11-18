eb28811045c169aa0caeb35486878464382048f3
root <api.test9989@gmail.com>
2024-11-18 12:08:42 +0000

    clean unneeded file & fix to my value

--- src/main/java/pockyProject/server/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package pockyProject.server.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pockyProject.server.entity.UserEntity;
-
-public interface UserRepository extends JpaRepository<UserEntity, String> {
-}
